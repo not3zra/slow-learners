35ddad0fac481b84e80b7233d373bc480f52b5d2
vyshnav-vinod <vyshnav.vinod003@gmail.com>
2025-03-24 21:32:14 +0530

    modify auth controller

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 
 // This is to allow the FE to get the session details of the backend.
-export function verifySession(req, res) {
+exports.verifySession = (req, res) =>{
     if (req.session.user) {
         res.status(200).json({ user: req.session.user });
     } else {
